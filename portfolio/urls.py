d0ac06213fdbe416bbd575906480a3d0d8aca286
tarang ranpara <tarangranpara1998@gmail.com>
2018-07-29 16:43:14 +0530

    got the image showing up working!

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .settings import MEDIA_ROOT,MEDIA_URL
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(MEDIA_URL, document_root = MEDIA_ROOT)
